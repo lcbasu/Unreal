cb539b8ddd92c728f4cc0aeb0fd64e6f74157504
Lokesh Basu <lokesh.basu@gmail.com>
2017-05-04 16:05:32 +0530

    feat: Adds a simple menu design project

---
+++ MenuDesign/Intermediate/Build/Win64/UE4Editor/Development/MenuDesign/PCH.MenuDesign.cpp
@@ -0,0 +1,1 @@
+#include "E:/Lokesh Basu/GitHub/Unreal/MenuDesign/Intermediate/Build/Win64/UE4Editor/Development/MenuDesign/PCH.MenuDesign.h"
---
+++ MenuDesign/Source/MenuDesign/MenuDesignGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MenuDesignGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MENUDESIGN_API AMenuDesignGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
